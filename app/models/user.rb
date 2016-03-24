86988aff47e5a5ecbea289740f7d24507a4d3824
Gorchel <kasatka567@gmail.com>
2016-03-24 22:49:22 +0300

    add role manager and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ActiveRecord::Base
-  before_create :assign_default_role
-
-  #protected :assign_default_role
 
   rolify
   # Include default devise modules. Others available are:
@@ -9,8 +6,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
-
-    def assign_default_role
-	    self.add_role "admin"
-	end
 end
