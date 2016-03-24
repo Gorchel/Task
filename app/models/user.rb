8dc60d8add7daa8315de2f405c84b0cc0e660811
Gorchel <kasatka567@gmail.com>
2016-03-24 12:48:35 +0300

    add Rale

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
+  before_create :assign_default_role
+
+  #protected :assign_default_role
+
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+    def assign_default_role
+	    self.add_role "admin"
+	end
 end
